92e81489a6440f0914b61f912177c751acc777ea
Daniel Houston <daniel.houston@uky.edu>
2019-04-05 09:24:29 -0400

    added new app

--- NativeSDKExamples/app/src/main/java/com/wikitude/samples/plugins/Camera.java
+++
@@ -1,9 +0,0 @@
-package com.wikitude.samples.plugins;
-
-interface Camera {
-
-    void start();
-    void stop();
-
-    int getCameraOrientation();
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-Read me for cs 499 final project.
